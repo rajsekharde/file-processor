017ed4534b0fcc695ef65ae3c7553ded898ae839
rajsekharde <rajsekharde612@gmail.com>
2026-06-08 17:40:13 +0530

    Add logging middleware to api

--- api/main.go
+++ api/main.go
@@ -5,17 +5,29 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"time"
 )
+
+func loggerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		
+		// Pass control to the next handler in the chain
+		next.ServeHTTP(w, r)
+		
+		log.Printf("%s %s %v\n", r.Method, r.URL.Path, time.Since(start))
+	})
+}
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintln(w, "API Server Running")
-	log.Printf("%s %s %d\n", r.Method, r.URL.Path, http.StatusOK)
 }
 
 func handleGetTask(w http.ResponseWriter, r *http.Request) {
 	resp, err := http.Get("http://localhost:8001/task")
 	var msg string
+
 	if err != nil {
 		log.Println(err.Error())
 		msg = "Could not fetch API"
@@ -26,17 +38,20 @@ func handleGetTask(w http.ResponseWriter, r *http.Request) {
 		}
 		msg = string(responseData)
 	}
+	defer resp.Body.Close()
 
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintln(w, msg)
-	log.Printf("%s %s %d\n", r.Method, r.URL.Path, http.StatusOK)
 }
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /", handleRoot)
-	mux.HandleFunc("GET /task", handleGetTask)
+	root := http.HandlerFunc(handleRoot)
+	task := http.HandlerFunc(handleGetTask)
+
+	mux.Handle("GET /", loggerMiddleware(root))
+	mux.Handle("GET /task", loggerMiddleware(task))
 
 	log.Println("API Server running on port 8000...")
 	http.ListenAndServe(":8000", mux)
