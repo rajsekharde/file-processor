9c862b6b41d3bca1edba1b79477717dc28483848
rajsekharde <rajsekharde612@gmail.com>
2026-06-08 16:39:51 +0530

    Add GET /task endpoint to worker server

--- worker/main.go
+++ worker/main.go
@@ -12,9 +12,18 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 	log.Printf("%s %s %d\n", r.Method, r.URL.Path, http.StatusOK)
 }
 
+func handleTask(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "Task Processed")
+	log.Printf("%s %s %d\n", r.Method, r.URL.Path, http.StatusOK)
+}
+
 func main() {
 	mux := http.NewServeMux()
+	
 	mux.HandleFunc("GET /", handleRoot)
+	mux.HandleFunc("GET /task", handleTask)
+
 	log.Println("Worker Server running on port 8001...")
 	http.ListenAndServe(":8001", mux)
 }
