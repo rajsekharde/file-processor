1f389780257b735eee15b751c703f9d6a11fe8e4
rajsekharde <rajsekharde612@gmail.com>
2026-06-08 17:01:32 +0530

    Call worker API endpoint through main API Server

--- api/main.go
+++ api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
@@ -12,9 +13,31 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 	log.Printf("%s %s %d\n", r.Method, r.URL.Path, http.StatusOK)
 }
 
+func handleGetTask(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get("http://localhost:8001/task")
+	var msg string
+	if err != nil {
+		log.Println(err.Error())
+		msg = "Could not fetch API"
+	} else {
+		responseData, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Println(err.Error())
+		}
+		msg = string(responseData)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, msg)
+	log.Printf("%s %s %d\n", r.Method, r.URL.Path, http.StatusOK)
+}
+
 func main() {
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("GET /", handleRoot)
+	mux.HandleFunc("GET /task", handleGetTask)
+
 	log.Println("API Server running on port 8000...")
 	http.ListenAndServe(":8000", mux)
 }
