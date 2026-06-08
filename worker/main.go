f0458c575fc9043dc0a5796fd19ba9e68176aa99
rajsekharde <rajsekharde612@gmail.com>
2026-06-08 16:34:49 +0530

    Set up basic worker server

--- worker/main.go
+++ worker/main.go
@@ -8,13 +8,13 @@ import (
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintln(w, "API Server Running")
+	fmt.Fprintln(w, "Worker Server Running")
 	log.Printf("%s %s %d\n", r.Method, r.URL.Path, http.StatusOK)
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /", handleRoot)
-	log.Println("API Server running on port 8000...")
-	http.ListenAndServe(":8000", mux)
+	log.Println("Worker Server running on port 8001...")
+	http.ListenAndServe(":8001", mux)
 }
