7de4bd0fd872dbf2bb761e62d7db78af770e70dc
rajsekharde <rajsekharde612@gmail.com>
2026-06-09 17:15:58 +0530

    Fix GetTask handler: add error checks

--- api/handlers.go
+++ api/handlers.go
@@ -14,20 +14,21 @@ func HandleRoot(w http.ResponseWriter, r *http.Request) {
 
 func HandleGetTask(w http.ResponseWriter, r *http.Request) {
 	resp, err := http.Get("http://localhost:8001/task")
-	var msg string
 
 	if err != nil {
 		log.Println(err.Error())
-		msg = "Could not fetch API"
-	} else {
-		responseData, err := io.ReadAll(resp.Body)
-		if err != nil {
-			log.Println(err.Error())
-		}
-		msg = string(responseData)
-		defer resp.Body.Close()
+		http.Error(w, "Could not fetch API", http.StatusBadGateway)
+		return
+	}
+	defer resp.Body.Close()
+
+	responseData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Could not read data", http.StatusInternalServerError)
+		return
 	}
 
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintln(w, msg)
+	fmt.Fprintln(w, string(responseData))
 }
