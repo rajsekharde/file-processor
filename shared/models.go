a3ca4bdb124ca24696930d6d8deb3e96625b451b
rajsekharde <rajsekharde612@gmail.com>
2026-08-30 12:06:25 +0530

    Add struct for resize task payload

--- shared/models.go
+++ shared/models.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// Redis hash
 type Task struct {
 	ID string `json:"id"`
 	Type string `json:"type"`
@@ -13,12 +14,21 @@ type Task struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
+// POST /task response
+type TaskRequest struct {
+	Type string `json:"type"`
+	Payload json.RawMessage `json:"payload"`
+}
+
+// Payload for convert format
 type ConvertFormatPayload struct {
 	FileName string `json:"file_name"`
 	OutputFormat string `json:"output_format"`
 }
 
-type TaskRequest struct {
-	Type string `json:"type"`
-	Payload json.RawMessage `json:"payload"`
+// Payload for resize image
+type ResizeImagePayload struct {
+	FileName string `json:"file_name"`
+	TargetWidth int `json:"target_width"`
+	TargetHeight int `json:"target_height"`
 }
